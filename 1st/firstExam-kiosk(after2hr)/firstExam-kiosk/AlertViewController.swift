76bfff4f7a64730d97e30b450135ae3071702051
roobing <roobing11@gmail.com>
2020-05-01 18:03:57 +0900

    First exam files
    end

---
+++ 1st/firstExam-kiosk(after2hr)/firstExam-kiosk/AlertViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AlertViewController.swift
+//  firstExam-kiosk
+//
+//  Created by Woobin Cheon on 2020/05/01.
+//  Copyright © 2020 Woobin Cheon. All rights reserved.
+//
+
+import UIKit
+
+class AlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("1")
+    }
+
+}
