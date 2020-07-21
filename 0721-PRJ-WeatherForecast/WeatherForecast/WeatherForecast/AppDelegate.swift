b2eda7d26a0b7ccf13cb1707a2fe2ff10bbe3204
roobing <roobing11@gmail.com>
2020-07-22 00:37:20 +0900

    미니프로젝트: 날씨앱 시작

---
+++ 0721-PRJ-WeatherForecast/WeatherForecast/WeatherForecast/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  WeatherForecast
+//
+//  Copyright © 2020 Giftbot. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
---
+++ 0721-PRJ-WeatherForecast/WeatherForecast/WeatherForecast/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  WeatherForecast
+//
+//  Copyright © 2020 Giftbot. All rights reserved.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    
+  }
+}
